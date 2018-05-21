f18a518a56920546ff5b991bb765f7d344413a17
Janet Isak <janetisak@yahoo.com>
2018-05-21 16:12:23 +0300

    test instantiating new hero

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,6 +1,28 @@
 public class Hero {
 
-  public Hero(String name) {
+  private String mName;
+  private int mAge;
+  private String mPower;
+  private String mWeakness;
 
+  public Hero(String name, int age, String power, String weakness) {
+
+    mName = name;
+    mAge = age;
+    mPower = power;
+    mWeakness = weakness;
   }
+
+  public String getName() {
+    return mName;
+  }
+  // public int getAge() {
+  //   return mAge;
+  // }
+  // public String getPower() {
+  //   return mPower;
+  // }
+  // public String getWeakness() {
+  //   return mWeakness;
+  // }
 }
--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -5,8 +5,14 @@ public class HeroTest {
 
   @Test
   public void Hero_instantiatesCorrectly_true() {
-    Hero newHero = new Hero("dad");
+    Hero newHero = new Hero("Spider Man", 30, "Flying Around", "Beautiful Girl");
     assertEquals(true, newHero instanceof Hero);
+  }
+
+  @Test
+  public void Hero_instantiatesWithName_String() {
+    Hero newHero = new Hero("Spider Man", 30, "Flying Around", "Beautiful Girl");
+    assertEquals("Spider Man", newHero.getName());
   }
 
 }
