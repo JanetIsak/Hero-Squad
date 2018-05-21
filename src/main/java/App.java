8e2f8135a010544f1fc49d1d7df5d26caf45701f
Janet Isak <janetisak@yahoo.com>
2018-05-21 17:38:39 +0300

    add post request and move resources folder inside main folder

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,11 +6,12 @@ import static spark.Spark.*;
 
 public class App {
   public static void main(String[] args) {
-    staticFileLocation("/public");
+    // staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
     get("/", (request, response) -> {
      Map<String, Object> model = new HashMap<String, Object>();
+     model.put("hero", request.session().attribute("hero"));
      model.put("template", "templates/index.vtl");
      return new ModelAndView(model, layout);
    }, new VelocityTemplateEngine());
@@ -22,11 +23,10 @@ public class App {
      int age = Integer.parseInt(request.queryParams("age"));
      String power = request.queryParams("power");
      String weakness = request.queryParams("weakness");
-
      Hero newHero = new Hero(name, age, power, weakness);
      request.session().attribute("hero", newHero);
 
-     model.put("template", "templates/details.vtl");
+     model.put("template", "templates/heroList.vtl");
      return new ModelAndView(model, layout);
    }, new VelocityTemplateEngine());
 
